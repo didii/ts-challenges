c150c88e11ad004de43d4f0c56554a34c69c0121
Dieter Van Broeck <dieter.vanbroeck@ordina.be>
2020-11-05 10:55:38 +0100

    Publisher

--- src/publisher/publisher.final.ts
+++ src/publisher/publisher.final.ts
@@ -1,25 +1,25 @@
 import { Observable, Subject } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import { Type } from './angular.core';
 
-export interface IPublisher {
-  publish(message: any): void;
-  listen(type: any): Observable<any>;
-}
-
-export class Publisher implements IPublisher {
+/** A typed event publisher */
+export class Publisher {
   private subject = new Subject<any>();
 
-  public publish(message: any): void {
-    // TODO: implement
+  /** Publishes a message of type T */
+  public publish<T extends Object>(message: T) {
+    console.log('Publishing "' + message.constructor.name + '" with payload', message);
+    this.subject.next(message);
   }
 
-  public listen(type: any): Observable<any> {
-    // TODO: implement
-    throw Error('Not implemented');
+  /** Listens to all messages of a certain type */
+  public listen<T extends Object>(type: Type<T>): Observable<T> {
+    return this.subject.pipe(filter(x => x.constructor === type));
   }
 
+  /** Closes all listeners, the publisher cannot be used anymore after this is called */
   public close() {
-    this.subject.unsubscribe();
+    this.subject.complete();
   }
 }
 
@@ -45,10 +45,11 @@ class FormSubmitMessage {
 
 let publisher = new Publisher();
 publisher.listen(NameChangedMessage).subscribe(x => console.log('Name changed:', x));
-publisher.listen(FormSubmitMessage).subscribe(x => console.log('Form submit firstName:', x.firstName));
-//                                                                                         ^ auto-complete
+publisher.listen(FormSubmitMessage).subscribe(x => console.log('Form submit:', x));
 
 publisher.publish(new NameChangedMessage({ name: 'trololoo' }));
+publisher.publish(new NameChangedMessage({ name: 'tralalaa' }));
 publisher.publish(new FormSubmitMessage({ firstName: 'rubber', lastName: 'duck', phone: '+32475123456' }));
+publisher.publish({ firstName: 'rubber', lastName: 'duck', phone: '+32475123456' });
 
 publisher.close();
